e614269ad01082c7497edf4d78a014febfec7c40
NikolMontes <a20203644@pucp.edu.pe>
2025-05-07 13:37:29 -0500

    USO DE FRAGMENTS

--- TELEWEATHER/app/src/main/java/com/example/tele_weather/AppActivity.java
+++ TELEWEATHER/app/src/main/java/com/example/tele_weather/AppActivity.java
@@ -1,24 +1,66 @@
 package com.example.tele_weather;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.tele_weather.databinding.ActivityAppBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AppActivity extends AppCompatActivity {
+    private ActivityAppBinding binding;
+    private NavController navController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_app);
+
+        binding = ActivityAppBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Configurar Navigation Component
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        // Configurar Bottom Navigation con Navigation Component
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+
+        /*binding.bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        // Navegar al destino correspondiente
+                        return NavigationUI.onNavDestinationSelected(item, navController);
+                    }
+                });*/
+
+    }
+
+    /*@Override
+    public void onBackPressed() {
+        finish(); // Cierra AppActivity y vuelve a MainActivity directamente
+    }*/
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Liberar el binding cuando la actividad se destruye
+        binding = null;
     }
 }
